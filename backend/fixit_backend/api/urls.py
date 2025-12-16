b3d7166918d6ecec87e100316bebbf6862f517e4
ITSROHITTECH <srivastavarohitkumar067@gmail.com>
2025-12-16 18:46:17 +0530

    add backend or working form for signup page

--- backend/fixit_backend/api/urls.py
+++ backend/fixit_backend/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import test_api
+from .views import test_api, signup
 
 urlpatterns = [
     path('test/', test_api),
+    path('signup/', signup),
 ]
+
+
+
--- backend/fixit_backend/api/views.py
+++ backend/fixit_backend/api/views.py
@@ -11,3 +11,28 @@ def test_api(request):
         "status": "success",
         "message": "Fixit backend connected successfully 🚀"
     })
+
+
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['POST'])
+def signup(request):
+    username = request.data.get('username')
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    if not username or not email or not password:
+        return Response({"error": "All fields are required"}, status=400)
+
+    if User.objects.filter(username=username).exists():
+        return Response({"error": "Username already exists"}, status=400)
+
+    user = User.objects.create_user(
+        username=username,
+        email=email,
+        password=password
+    )
+
+    return Response({"message": "Signup successful"})
