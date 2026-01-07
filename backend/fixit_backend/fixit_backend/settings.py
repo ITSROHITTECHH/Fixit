42068c73aa9746bc9d55ae3d15b67ea6f35f4dfb
ITSROHITTECH <srivastavarohitkumar067@gmail.com>
2026-01-07 23:34:08 +0530

    .

--- backend/fixit_backend/fixit_backend/settings.py
+++ backend/fixit_backend/fixit_backend/settings.py
@@ -160,7 +160,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://127.0.0.1:3000",
-#     "http://localhost:3000",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
