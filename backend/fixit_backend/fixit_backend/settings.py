42ee4e7303f26087fa8e8b63ff787d5134069083
ITSROHITTECH <srivastavarohitkumar067@gmail.com>
2026-03-10 17:35:01 +0530

    manage cors

--- backend/fixit_backend/fixit_backend/settings.py
+++ backend/fixit_backend/fixit_backend/settings.py
@@ -67,13 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 
@@ -170,5 +164,5 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "https://fixitttt.vercel.app"
+    "https://fixit-sable.vercel.app/"
 ]
